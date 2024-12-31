6456fac016f87d508b77e04304d84078967e95b3
elizailtnere <eliza.iltnere@gmail.com>
2024-12-31 10:28:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,96 +2,96 @@ def start():
     print("Tu esi sagatavojies apzagt māju. Tu esi saņēmis informāciju par mājas īpašniekiem un to vērtīgo mantu atrašanās vietām.")
     print("Tagad tev jāizlemj, kā iekļūt mājā.")
     
-    choice = input("1. Izmanto durvis un mēģini iekļūt caur ieejas durvīm.\n2. Izmanto logu, lai iekļūtu mājā.\n3. Meklē pagrabu.\nIzvēlies (1/2/3): ")
+    izvele = input("1. Izmanto durvis un mēģini iekļūt caur ieejas durvīm.\n2. Izmanto logu, lai iekļūtu mājā.\n3. Meklē pagrabu.\nIzvēlies (1/2/3): ")
 
-    if choice == "1":
-        through_door()
-    elif choice == "2":
-        through_window()
-    elif choice == "3":
-        through_basement()
+    if izvele == "1":
+        caur_durvim()
+    elif izvele == "2":
+        caur_logu()
+    elif izvele == "3":
+        caur_pagrabu()
     else:
         print("Nederīga izvēle. Mēģini vēlreiz.")
         start()
 
-def through_door():
+def caur_durvim():
     print("\nTu mēģini atvērt durvis, bet tās ir aizslēgtas. Tomēr redzi, ka durvju blakus ir logi, kas varētu būt vaļā.")
-    choice = input("1. Pārbaudi, vai logi ir atvērti.\n2. Mēģini izsist durvis.\nIzvēlies (1/2): ")
+    izvele = input("1. Pārbaudi, vai logi ir atvērti.\n2. Mēģini izsist durvis.\nIzvēlies (1/2): ")
 
-    if choice == "1":
-        through_window()
-    elif choice == "2":
-        break_door()
+    if izvele == "1":
+        caur_logu()
+    elif izvele == "2":
+        lauzt_durvis()
     else:
         print("Nederīga izvēle. Mēģini vēlreiz.")
-        through_door()
+        caur_durvim()
 
-def break_door():
+def lauzt_durvis():
     print("\nTu izsist durvis, bet trokšņojot pievērš uzmanību kaimiņiem. Policija ierodas, un tu esi aizturēts. Beigas!")
     
-def through_window():
+def caur_logu():
     print("\nTu izdari veiklu pāreju caur logu un ieej mājā. Tagad esi iekšā.")
     print("Iekšā ir tumsā, un tu dzirdi soļus, kas nāk no augšas. Kas jādara?")
-    choice = input("1. Steidzīgi ej uz augšu, lai atrastu dārgumus.\n2. Paliec klusumā un izgaist, ja kāds atnāks.\nIzvēlies (1/2): ")
+    izvele = input("1. Steidzīgi ej uz augšu, lai atrastu dārgumus.\n2. Paliec klusumā un izgaist, ja kāds atnāks.\nIzvēlies (1/2): ")
 
-    if choice == "1":
-        upstairs_find_treasure()
-    elif choice == "2":
-        hide()
+    if izvele == "1":
+        augsa_dargumi()
+    elif izvele == "2":
+        slepties()
     else:
         print("Nederīga izvēle. Mēģini vēlreiz.")
-        through_window()
+        caur_logu()
 
-def upstairs_find_treasure():
+def augsa_dargumi():
     print("\nTu nokļūsti augšā, kur atradīsi vērtīgu dārgumu kasti. Taču pie viena dārguma skapja pēkšņi ieslēdzas trauksmes signāls!")
-    choice = input("1. Ņem dārgumus un steidzīgi bēdz!\n2. Pārtrauc, saproti, ka risks nav vērts, un meklē citu vietu.\nIzvēlies (1/2): ")
+    izvele = input("1. Ņem dārgumus un steidzīgi bēdz!\n2. Pārtrauc, saproti, ka risks nav vērts, un meklē citu vietu.\nIzvēlies (1/2): ")
 
-    if choice == "1":
-        get_arrested()
-    elif choice == "2":
-        return_to_basement()
+    if izvele == "1":
+        tiec_arestets()
+    elif izvele == "2":
+        atgreizies_pagraba()
     else:
         print("Nederīga izvēle. Mēģini vēlreiz.")
-        upstairs_find_treasure()
+        augsa_dargumi()
 
-def get_arrested():
+def tiec_arestets():
     print("\nTu paķeri dārgumus, bet trauksmes signāls piesaista policiju. Tevi aiztur un nogādā cietumā. Beigas!")
     
-def return_to_basement():
+def atgreizies_pagraba():
     print("\nTu pārtrauc un atgriezies pagrabiņā, bet, kad kāp laukā no pagraba, tu dzirdi soļus. Policija ierodas. Beigas!")
 
-def hide():
+def slepties():
     print("\nTu paliec klusumā, bet, izdzirdot soļus, tu nolemj bēgt. Diemžēl pie durvīm tu sastopi mājas īpašnieku.")
-    choice = input("1. Izsist mājas īpašniekam no rokām ieroci un bēdz!\n2. Pārsteidz mājas īpašnieku ar mieru un cenšoties bēgt.\nIzvēlies (1/2): ")
+    izvele = input("1. Izsist mājas īpašniekam no rokām ieroci un bēdz!\n2. Pārsteidz mājas īpašnieku ar mieru un cenšoties bēgt.\nIzvēlies (1/2): ")
 
-    if choice == "1":
-        get_arrested()
-    elif choice == "2":
-        escape()
+    if izvele == "1":
+        tiec_arestets()
+    elif izvele == "2":
+        izklut()
     else:
         print("Nederīga izvēle. Mēģini vēlreiz.")
-        hide()
+        slepties()
 
-def escape():
+def izklut():
     print("\nTu veiksmīgi iznāc no mājas, bet policija tev sāk sekot. Tava bēgšana beidzas ar aizturēšanu. Beigas!")
 
-def through_basement():
+def caur_pagrabu():
     print("\nTu pieej pagraba logiem un nolemj ieiet pa pagrabu. Tu dzirdi suņa riešanu, taču tu esi uzmanīgs.")
-    choice = input("1. Klusām ieej un tuvojies slēptajam dārgumam.\n2. Ej ārā no pagraba, jo tu esi dzirdējis kādu skrienam.\nIzvēlies (1/2): ")
+    izvele = input("1. Klusām ieej un tuvojies slēptajam dārgumam.\n2. Ej ārā no pagraba, jo tu esi dzirdējis kādu skrienam.\nIzvēlies (1/2): ")
 
-    if choice == "1":
-        find_basement_treasure()
-    elif choice == "2":
-        get_caught_by_dog()
+    if izvele == "1":
+        atrast_dargumi_pagraba()
+    elif izvele == "2":
+        sunis_noker()
     else:
         print("Nederīga izvēle. Mēģini vēlreiz.")
-        through_basement()
+        caur_pagrabu()
 
-def find_basement_treasure():
+def atrast_dargumi_pagraba():
     print("\nTu atrod dārgumus un apmierināts ar panākumiem pamet māju. Taču, kad tu domā par aizbēgšanu, tevi ielenc policija.")
     print("Beigas!")
 
-def get_caught_by_dog():
+def sunis_noker():
     print("\nPagrabā ir suns, kas uzbrūk tev, kad tu mēģini iziet. Tevi noķer un aiztur. Beigas!")
 
 # Spēles sākums
